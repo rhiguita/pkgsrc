407131833c403c385a02d8044e0afdec5e3fec6c
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-03 18:27:36 -0700

    Fixup fromcvs/togit conversion

--- lang/gcc46/hacks.mk
+++ lang/gcc46/hacks.mk
@@ -1,10 +1,9 @@
-# $NetBSD$
+# $NetBSD: hacks.mk,v 1.1 2013/04/02 22:31:40 jmcneill Exp $
 
 #
 # Avoid -fkeep-inline-functions with NetBSD/arm
 #
 .if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "arm"
 PKG_HACKS+=		netbsd-arm-stage1-fix
-USE_LANGUAGES+=		c c++
-CONFIGURE_ARGS+=	--enable-build-with-cxx
+CONFIGURE_ARGS+=	--disable-bootstrap
 .endif
