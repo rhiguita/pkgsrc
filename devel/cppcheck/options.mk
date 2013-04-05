364d5500ded48910a5218f3339042187c8d20787
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-05 12:54:25 -0700

    Fixup fromcvs/togit conversion

--- devel/cppcheck/options.mk
+++ devel/cppcheck/options.mk
@@ -1,15 +1,17 @@
-# $NetBSD: options.mk,v 1.3 2011/05/29 11:52:12 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2012/11/29 13:52:51 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cppcheck
 PKG_SUPPORTED_OPTIONS=	qt
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	qt
+
 .if !empty(PKG_OPTIONS:Mqt)
-PLIST_SRC+=		PLIST PLIST.qt
+PLIST.qt=	yes
 
 .include "../../x11/qt4-libs/buildlink3.mk"
-BUILDLINK_DEPMETHOD.qt4-tools?= full
+BUILDLINK_DEPMETHOD.qt4-tools+= full
 .include "../../x11/qt4-tools/buildlink3.mk"
 
 post-build:
