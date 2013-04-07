32a7aab7ec67142fd32b9148e7b78f95f09a182e
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-06 18:28:51 -0700

    Fixup fromcvs/togit conversion

--- chat/xchat/options.mk
+++ chat/xchat/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2009/10/24 15:51:25 tnn Exp $
+# $NetBSD: options.mk,v 1.15 2012/06/16 23:06:07 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat
 PKG_SUPPORTED_OPTIONS=	inet6 ssl socks5 mitshm gnome
@@ -50,8 +50,8 @@ CONFIGURE_ARGS+=	--disable-shm
 CONFIGURE_ARGS+=	--enable-dbus
 PLIST.dbus=		yes
 SUBST_CLASSES+=		gconf
-SUBST_STAGE.gconf+=	pre-configure
-SUBST_MESSAGE.gconf+=	Disabling schema registration/installation.
+SUBST_STAGE.gconf=	pre-configure
+SUBST_MESSAGE.gconf=	Disabling schema registration/installation.
 SUBST_SED.gconf+=	-e 's,DO_GCONF_TRUE,DO_GCONF_FALSE,g'
 SUBST_FILES.gconf+=	src/common/dbus/Makefile.in
 .	include "../../devel/GConf/schemas.mk"
