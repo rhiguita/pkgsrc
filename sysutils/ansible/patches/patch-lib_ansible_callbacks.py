3f50f5a34f9fccf1a57b0b409b8aba9644bb4624
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-13 14:58:47 -0700

    Fixup fromcvs/togit conversion

--- sysutils/ansible/patches/patch-lib_ansible_callbacks.py
+++ sysutils/ansible/patches/patch-lib_ansible_callbacks.py
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- lib/ansible/callbacks.py.orig	2013-01-26 16:50:21.000000000 +0000
-+++ lib/ansible/callbacks.py
-@@ -31,9 +31,9 @@ elif os.path.exists("/usr/bin/cowsay"):
+--- lib/ansible/callbacks.py.orig	2013-03-30 16:29:28.000000000 -0700
++++ lib/ansible/callbacks.py	2013-04-13 09:05:36.000000000 -0700
+@@ -30,9 +30,9 @@
      cowsay = "/usr/bin/cowsay"
  elif os.path.exists("/usr/games/cowsay"):
      cowsay = "/usr/games/cowsay"
@@ -11,6 +11,6 @@ $NetBSD$
      # BSD path for cowsay
 -    cowsay = "/usr/local/bin/cowsay"
 +    cowsay = "@LOCALBASE@/bin/cowsay"
- 
- def call_callback_module(method_name, *args, **kwargs):
- 
+ elif os.path.exists("/opt/local/bin/cowsay"):
+     # MacPorts path for cowsay
+     cowsay = "/opt/local/bin/cowsay"
