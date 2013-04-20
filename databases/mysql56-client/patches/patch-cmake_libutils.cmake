f8c00916e0149bb3fcaf1b79addf60f2a4c2dbe4
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-20 06:36:25 -0700

    Fixup fromcvs/togit conversion

--- databases/mysql56-client/patches/patch-cmake_libutils.cmake
+++ databases/mysql56-client/patches/patch-cmake_libutils.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-cmake_libutils.cmake,v 1.1.1.1 2011/04/25 21:12:53 adam Exp $
+$NetBSD: patch-cmake_libutils.cmake,v 1.1 2013/02/13 21:00:04 adam Exp $
 
 On Mac OS X, use PkgSrc libtool.
 
 --- cmake/libutils.cmake.orig	2011-04-05 12:38:41.000000000 +0000
 +++ cmake/libutils.cmake
-@@ -178,7 +178,7 @@ MACRO(MERGE_STATIC_LIBS TARGET OUTPUT_NA
+@@ -179,7 +179,7 @@ MACRO(MERGE_STATIC_LIBS TARGET OUTPUT_NA
        "${LINKER_EXTRA_FLAGS}")
    ELSE()
      GET_TARGET_PROPERTY(TARGET_LOCATION ${TARGET} LOCATION)  
--- databases/mysql56-client/patches/patch-sql_mysqld.cc
+++ databases/mysql56-client/patches/patch-sql_mysqld.cc
@@ -1,4 +1,4 @@
-$NetBSD$
+$NetBSD: patch-sql_mysqld.cc,v 1.1 2013/02/13 21:00:07 adam Exp $
 
 --- sql/mysqld.cc.orig	2013-01-22 16:54:50.000000000 +0000
 +++ sql/mysqld.cc
@@ -20,12 +20,3 @@ $NetBSD$
    /* We can't handle floating point exceptions with threads, so disable
       this on freebsd
       Don't fall for overflow, underflow,divide-by-zero or loss of precision.
-@@ -6292,7 +6292,7 @@ void handle_connections_sockets()
-             The connection was refused by TCP wrappers.
-             There are no details (by client IP) available to update the host_cache.
-           */
--          statistic_increment(connection_tcpwrap_errors, &LOCK_status);
-+          statistic_increment(connection_errors_tcpwrap, &LOCK_status);
-           continue;
-         }
-       }
