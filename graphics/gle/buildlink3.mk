9f03c45ff062d85038ad0e93922cbed8de69bec1
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-04-24 09:27:13 -0700

    Fixup fromcvs/togit conversion

--- graphics/gle/buildlink3.mk
+++ graphics/gle/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:10:51 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:39 joerg Exp $
 
 BUILDLINK_TREE+=	gle
 
@@ -9,7 +9,12 @@ BUILDLINK_API_DEPENDS.gle+=		gle>=3.0.3
 BUILDLINK_ABI_DEPENDS.gle+=	gle>=3.1.0nb2
 BUILDLINK_PKGSRCDIR.gle?=	../../graphics/gle
 
-.include "../../graphics/Mesa/buildlink3.mk"
+# GLUT is used only by the examples, which aren't built or installed.
+#.include "../../graphics/glut/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 .endif # GLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gle
