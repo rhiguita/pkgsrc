a6e37fa63406b6a7beb86dc943d8be6a39fae206
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-05-08 12:29:14 -0700

    Fixup fromcvs/togit conversion

--- graphics/ilmbase/patches/patch-Iex_IexThrowErrnoExc.cpp
+++ graphics/ilmbase/patches/patch-Iex_IexThrowErrnoExc.cpp
@@ -1,8 +1,8 @@
-$NetBSD$
+$NetBSD: patch-Iex_IexThrowErrnoExc.cpp,v 1.1 2012/07/26 10:58:47 bsiegert Exp $
 
 --- Iex/IexThrowErrnoExc.cpp.orig	Thu Jul 26 10:46:21 2012
 +++ Iex/IexThrowErrnoExc.cpp
-@@ -410,7 +410,7 @@ void throwErrnoExc (const std::string &t
+@@ -419,7 +419,7 @@ void throwErrnoExc (const std::string &t
  	    throw EnametoolongExc (tmp);
        #endif
  
